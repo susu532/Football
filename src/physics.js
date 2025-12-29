945902d9b5378c34a75f68530d27b7c4ed36d9fb
mimi489 <nnnrrrtttsss123@gmail.com>
2025-12-29 15:38:46 +0100

    update

--- src/physics.js
+++ src/physics.js
@@ -76,7 +76,7 @@ export function createWorld() {
   groundBody = new CANNON.Body({
     mass: 0,
     material: groundMaterial,
-    position: new CANNON.Vec3(0, 0.1, 0),
+    position: new CANNON.Vec3(0, 0.57, 0),
   })
   groundBody.addShape(groundShape)
   groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
