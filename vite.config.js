304b3dfe784245cee0999c3c71d96f041d6c92dc
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-01 11:33:53 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-three': ['three', '@react-three/fiber', '@react-three/drei'],
+          'vendor-physics': ['cannon-es'],
+          'vendor-react': ['react', 'react-dom', 'zustand'],
+        }
+      }
+    }
+  }
 })
