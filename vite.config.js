b6fa2979573f1b40d9f754b9f9a1b65a6c257a8e
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-03 19:13:58 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 2500,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'vendor-three': ['three'],
-          'vendor-r3f': ['@react-three/fiber', '@react-three/drei'],
-          'vendor-rapier': ['@dimforge/rapier3d-compat'],
-          'vendor-physics-core': ['@react-three/rapier', 'cannon-es'],
-          'vendor-playroom': ['playroomkit'],
-          'vendor-react-core': ['react', 'react-dom', 'zustand'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@dimforge/rapier3d-compat')) return 'vendor-rapier-wasm'
+            if (id.includes('@react-three/rapier')) return 'vendor-r3-rapier'
+            if (id.includes('three')) return 'vendor-three'
+            if (id.includes('@react-three/drei')) return 'vendor-drei'
+            if (id.includes('@react-three/fiber')) return 'vendor-fiber'
+            if (id.includes('playroomkit')) return 'vendor-playroom'
+            if (id.includes('react') || id.includes('react-dom') || id.includes('scheduler')) return 'vendor-react'
+            return 'vendor-others'
+          }
         }
       }
     }
