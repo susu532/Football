4c216581e337878ab36276fb1ebd0b2dca9a9cb4
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-06 18:03:30 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'Soccer 3D Experience',
         short_name: 'Soccer 3D',
