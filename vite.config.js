7916ae74b7f50e8848186070fa21f9c7ce257392
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-18 12:19:02 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -45,13 +45,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'vendor-three': ['three'],
-          'vendor-r3f': ['@react-three/fiber', '@react-three/drei'],
-          'vendor-rapier': ['@dimforge/rapier3d-compat'],
-          'vendor-physics-core': ['@react-three/rapier', 'cannon-es'],
-          'vendor-react-core': ['react', 'react-dom', 'zustand'],
-        }
+        manualChunks: undefined
       }
     }
   }
