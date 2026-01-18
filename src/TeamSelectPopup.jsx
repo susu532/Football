fb1c1c4bde0eb2d18a9c6d2473be5f39a28bcd49
mimi489 <nnnrrrtttsss123@gmail.com>
2026-01-18 16:04:39 +0100

    update

--- src/TeamSelectPopup.jsx
+++ src/TeamSelectPopup.jsx
@@ -513,6 +513,7 @@ export default function TeamSelectPopup({ defaultName, rooming }) {
         {view === 'create' && renderCreateView()}
         {view === 'customize' && renderCustomizeView()}
         {view === 'gamemodes' && renderGameModesView()}
+        {view === 'join' && renderJoinView()}
 
         {/* Right Sidebar - Rooms */}
         <div className="lobby-sidebar-right">
